449e1f95998701bb79ac3c054dbb89d79bc443d1
taehwee_park <taehwee_park@tmax.co.kr>
2022-05-02 15:23:21 +0900

    [Persistence Layer] Repository Interface 생성

--- src/main/java/com/example/demo/persistence/TodoRepository.java
+++ src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -4,5 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.demo.model.TodoEntity;
 
-public class TodoRepository {
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+
 }
