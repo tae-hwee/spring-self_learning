d436f01e6da6ed4ab7a01c02f7a811ed95d7d2c1
taehwee_park <taehwee_park@tmax.co.kr>
2022-09-22 17:55:15 +0900

    [Persistence Layer] 기본적인 Query 작성 방식
    description:
    - method를 활용한 기본적 Query 작성
    - @Query annotation을 활용하여 직접 Query 작성

--- src/main/java/com/example/demo/persistence/TodoRepository.java
+++ src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -6,5 +6,12 @@ import com.example.demo.model.TodoEntity;
 
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+    // ?1은 method의 매개변수 순서 위치 의미함
+    @Query("SELECT * FROM Todo t WHERE t.userId = ?1")
+    List<TodoEntity> findByUserId(String userId);
+}
 
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
+    List<TodoEntity> findByUserId(String userId);
 }
